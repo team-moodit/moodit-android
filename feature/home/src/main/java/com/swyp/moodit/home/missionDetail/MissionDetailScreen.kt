c006bc2bd7be3fc10659b6b0950da774d720b77d
User <mk43313@gmail.com>
2026-06-14 04:28:00 +0900

    feat: 미션 완료 버튼 클릭시 BottomSheet 출력 (#5)

--- feature/home/src/main/java/com/swyp/moodit/home/missionDetail/MissionDetailScreen.kt
+++ feature/home/src/main/java/com/swyp/moodit/home/missionDetail/MissionDetailScreen.kt
@@ -2,21 +2,34 @@ package com.swyp.moodit.home.missionDetail
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MissionDetailScreen(
     uiState: MissionDetailContract.State
 ) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -25,8 +38,32 @@ fun MissionDetailScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "MissionDetailScreen", style = MaterialTheme.typography.displayMedium)
-        Button(modifier = Modifier.fillMaxWidth(), onClick = {}) {
+        Button(modifier = Modifier.fillMaxWidth(), onClick = { showBottomSheet = true }) {
             Text(text = if (uiState.isCompleted) "만족도 입력하기" else "미션 완료")
+        }
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState,
+            containerColor = MaterialTheme.colorScheme.surface
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f)
+                    .navigationBarsPadding(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "직접 미션을 해보니 어땠나요?", style = MaterialTheme.typography.titleLarge)
+                Button(modifier = Modifier.fillMaxWidth(), onClick = {
+                    showBottomSheet = false
+                }) {
+                    Text(text = "완료")
+                }
+            }
         }
     }
 }
