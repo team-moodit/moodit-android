c9f0de6de0eff2ebb4dc41e4cd685a055d1a6cbe
User <mk43313@gmail.com>
2026-06-14 19:56:02 +0900

    feat: 단일 스크린 기반 취향 토너먼트 기능 및 UI 골격 구현 (#5) - Queue를 활용한 대진표 생성, 라운드 전환 및 부전승 처리 로직 구현 - 단일 Screen 내에서 상태에 따라 사진 선택 / 이유 선택 단계를 스위칭하는 Multi Step 구조 적용

--- feature/tournament/src/main/java/com/swyp/moodit/tournament/matchUp/MatchUpRoute.kt
+++ feature/tournament/src/main/java/com/swyp/moodit/tournament/matchUp/MatchUpRoute.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import timber.log.Timber
 
 @Composable
 fun MatchUpRoute(
@@ -16,6 +18,18 @@ fun MatchUpRoute(
     onShowSnackbar: suspend (String, String?) -> Boolean
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = viewModel.sideEffect) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                is MatchUpContract.SideEffect.NavigateToResult -> {
+                    Timber.d("NavigateToResult: ${sideEffect.winnerPhotoId}")
+                }
+                is MatchUpContract.SideEffect.NavigateBack -> {}
+                is MatchUpContract.SideEffect.ShowSnackbar -> {}
+            }
+        }
+    }
 
     when {
         uiState.isLoading -> {
@@ -28,7 +42,23 @@ fun MatchUpRoute(
         }
 
         else -> {
-            MatchUpScreen()
+            MatchUpScreen(
+                uiState = uiState,
+                onCandidateSelect = { candidate ->
+                    viewModel.sendIntent(
+                        MatchUpContract.Intent.OnCandidateSelect(
+                            candidate
+                        )
+                    )
+                },
+                onReasonSelect = { reasonId ->
+                    viewModel.sendIntent(
+                        MatchUpContract.Intent.OnReasonSelect(
+                            reasonId
+                        )
+                    )
+                }
+            )
         }
     }
 }
