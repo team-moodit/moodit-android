8820caf75d9959b1d50e15266ee40cb2c9990c04
User <mk43313@gmail.com>
2026-06-16 22:34:03 +0900

    feat: BaseResponse 공통 처리를 위한 getOrThrow 확장 함수 추가 (#7)

--- core/network/src/main/java/com/swyp/moodit/network/model/BaseResponse.kt
+++ core/network/src/main/java/com/swyp/moodit/network/model/BaseResponse.kt
@@ -1,5 +1,7 @@
 package com.swyp.moodit.network.model
 
+import retrofit2.Response
+
 data class BaseResponse<T>(
     val success: T? = null,
     val error: Error? = null
@@ -9,3 +11,15 @@ data class Error(
     val errorCode: String,
     val message: String
 )
+
+inline fun <T> Response<BaseResponse<T>>.getOrThrow(): T {
+    if (!this.isSuccessful) {
+        throw Exception("서버 통신 실패: ${this.code()}")
+    }
+    val baseResponse = this.body() ?: throw Exception("서버 응답 바디가 비어있습니다.")
+    if (baseResponse.success != null) {
+        return baseResponse.success
+    } else {
+        throw Exception(baseResponse.error?.message ?: "알 수 없는 서버 에러")
+    }
+}
