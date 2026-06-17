a40b4b15622c34181f9854a8c68a96721d4e6cbb
User <mk43313@gmail.com>
2026-06-17 18:56:20 +0900

    feat: UserPreferencesDataStore 구현 (#7) - 자동 로그인 여부, 온보딩 완료 여부에 대한 조회, UpSert 기능 구현

--- core/datastore/src/main/java/com/swyp/moodit/datastore/PreferencesKey.kt
+++ core/datastore/src/main/java/com/swyp/moodit/datastore/PreferencesKey.kt
@@ -1,5 +1,6 @@
 package com.swyp.moodit.datastore
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
@@ -7,4 +8,6 @@ internal object PreferencesKey {
     val ACCESS_TOKEN_KEY = stringPreferencesKey("moodit_jwt_token")
     val REFRESH_TOKEN_KEY = stringPreferencesKey("moodit_jwt_refresh_token")
     val USER_ID = longPreferencesKey("moodit_user_id")
+    val OnBoarding_Completed = booleanPreferencesKey("moodit_onboarding_completed")
+    val AutoLogin_Enabled = booleanPreferencesKey("moodit_auto_login_enabled")
 }
