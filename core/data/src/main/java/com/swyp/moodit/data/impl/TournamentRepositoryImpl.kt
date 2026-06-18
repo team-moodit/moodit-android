f7783ca275a1c262b2ff949d62ce9dbc6b1cb506
User <mk43313@gmail.com>
2026-06-19 02:46:15 +0900

    feat: TournamentRepository Hilt DI 추가 (#9)

---
+++ core/data/src/main/java/com/swyp/moodit/data/impl/TournamentRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package com.swyp.moodit.data.impl
+
+import com.swyp.moodit.data.repository.TournamentRepository
+import javax.inject.Inject
+
+internal class TournamentRepositoryImpl @Inject constructor(): TournamentRepository {
+}
---
+++ core/data/src/main/java/com/swyp/moodit/data/repository/TournamentRepository.kt
@@ -0,0 +1,4 @@
+package com.swyp.moodit.data.repository
+
+interface TournamentRepository {
+}
