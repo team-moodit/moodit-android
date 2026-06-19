8ba4bc93352e577296eb0305d7cc63b62cd8c4df
User <mk43313@gmail.com>
2026-06-20 02:30:05 +0900

    feat: TournamentRepository 이미지 업로드 기능 구현 (#9)

--- core/data/src/main/java/com/swyp/moodit/data/impl/TournamentRepositoryImpl.kt
+++ core/data/src/main/java/com/swyp/moodit/data/impl/TournamentRepositoryImpl.kt
@@ -1,7 +1,36 @@
 package com.swyp.moodit.data.impl
 
+import com.swyp.moodit.common.util.ImageProcessor
+import com.swyp.moodit.common.util.Result
 import com.swyp.moodit.data.repository.TournamentRepository
+import com.swyp.moodit.model.PartType
+import com.swyp.moodit.model.SelectedPhoto
+import com.swyp.moodit.model.UploadStatus
+import com.swyp.moodit.network.api.MooditApi
+import com.swyp.moodit.network.model.getOrThrow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 import javax.inject.Inject
 
-internal class TournamentRepositoryImpl @Inject constructor(): TournamentRepository {
+internal class TournamentRepositoryImpl @Inject constructor(
+    private val imageProcessor: ImageProcessor,
+    private val mooditApi: MooditApi
+) : TournamentRepository {
+    override suspend fun uploadImage(photo: SelectedPhoto): Result<SelectedPhoto> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val filePart = imageProcessor.toMultiPartBody(photo.uri)
+                    ?: throw IllegalArgumentException("파일 변환 실패")
+                val response = mooditApi.uploadFile(resourceType = PartType.MATCH.name, file = filePart).getOrThrow()
+                val uploadedPhoto = photo.copy(
+                    serverId = response.id,
+                    status = UploadStatus.Success(response.fileUrl)
+                )
+                Result.Success(uploadedPhoto)
+            } catch (e: Exception) {
+                Result.Error(e)
+            }
+        }
+    }
 }
--- core/data/src/main/java/com/swyp/moodit/data/repository/TournamentRepository.kt
+++ core/data/src/main/java/com/swyp/moodit/data/repository/TournamentRepository.kt
@@ -1,4 +1,8 @@
 package com.swyp.moodit.data.repository
 
+import com.swyp.moodit.common.util.Result
+import com.swyp.moodit.model.SelectedPhoto
+
 interface TournamentRepository {
+    suspend fun uploadImage(photo: SelectedPhoto): Result<SelectedPhoto>
 }
