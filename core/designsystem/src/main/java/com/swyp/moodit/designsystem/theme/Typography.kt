b67a5cb29be9029e3aa1938a58480d39f6eb94be
User <mk43313@gmail.com>
2026-06-23 02:40:08 +0900

    feat: MooditTypography 구현 (#11)

--- core/designsystem/src/main/java/com/swyp/moodit/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/java/com/swyp/moodit/designsystem/theme/Typography.kt
@@ -4,7 +4,8 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.swyp.moodit.designsystem.R
 
 val pretendardFontFamily = FontFamily(
@@ -23,7 +24,117 @@ val pretendardFontFamily = FontFamily(
 data class MooditTextStyle(
     val fontFamily: FontFamily = pretendardFontFamily,
     val fontWeight: FontWeight = FontWeight.Medium,
-    val fontSize: Dp = Dp.Unspecified,
-    val lineHeight: Dp = Dp.Unspecified,
-    val letterSpacing: Dp = Dp.Unspecified
+    val fontSize: TextUnit = TextUnit.Unspecified,
+    val lineHeight: TextUnit = TextUnit.Unspecified,
+    val letterSpacing: TextUnit = TextUnit.Unspecified
 )
+
+@Immutable
+data class MooditTypography(
+    val h1: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = (-0.48).sp
+    ),
+
+    val h2: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 32.sp,
+        letterSpacing = (-0.4).sp
+    ),
+
+    val h3: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val h4: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b1Large: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b1Medium: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.32).sp
+    ),
+
+    val b1Small: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.28).sp
+    ),
+
+    val b2Large: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b2Medium: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b2Small: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b2ExtraSmall: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b3Large: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b3Medium: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val b3Small: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    val caption: MooditTextStyle = MooditTextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 12.sp,
+        letterSpacing = 0.sp
+    )
+)
+
+val AppTypography = MooditTypography()
