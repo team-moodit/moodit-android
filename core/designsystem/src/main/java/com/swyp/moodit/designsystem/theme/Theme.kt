c2a0f569d12c37174d3e4ac91310316f108cd9c2
User <mk43313@gmail.com>
2026-06-23 02:40:15 +0900

    feat: MooditTheme 구현 (#11)

---
+++ core/designsystem/src/main/java/com/swyp/moodit/designsystem/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.swyp.moodit.designsystem.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+internal val LocalMooditColorScheme = staticCompositionLocalOf { lightColorScheme }
+internal val LocalMooditTypography = staticCompositionLocalOf { AppTypography }
+
+object MooditTheme {
+    val colors: CustomColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMooditColorScheme.current
+
+    val typography: MooditTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMooditTypography.current
+}
+
+@Composable
+fun MooditTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (isDarkTheme) darkColorScheme else lightColorScheme
+
+    CompositionLocalProvider(
+        LocalMooditColorScheme provides colors,
+        LocalMooditTypography provides AppTypography,
+        content = content,
+    )
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val window = (view.context as Activity).window
+        val insetsController = WindowCompat.getInsetsController(window, view)
+        insetsController.isAppearanceLightNavigationBars = !isDarkTheme
+        insetsController.isAppearanceLightStatusBars = !isDarkTheme
+    }
+}
