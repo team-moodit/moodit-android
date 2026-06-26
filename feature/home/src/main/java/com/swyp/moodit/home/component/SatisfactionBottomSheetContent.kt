c866c42f886297cfe59257f7d329bc35896c7fd0
User <mk43313@gmail.com>
2026-06-26 17:04:23 +0900

    feat: SatisfactionBottomSheetContent UI 구현 (#19)

---
+++ feature/home/src/main/java/com/swyp/moodit/home/component/SatisfactionBottomSheetContent.kt
@@ -0,0 +1,157 @@
+package com.swyp.moodit.home.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.swyp.moodit.designsystem.component.MooditSlider
+import com.swyp.moodit.designsystem.component.button.MooditFilledButton
+import com.swyp.moodit.designsystem.theme.MooditTheme
+
+@Composable
+fun SatisfactionBottomSheetContent(
+    currentSliderRating: Float,
+    onValueChange: (Float) -> Unit,
+    onCompleteClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .navigationBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "직접 미션을 해보니 어땠나요?",
+            style = MooditTheme.typography.h2,
+            color = MooditTheme.colors.onBackground
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .border(
+                    1.dp,
+                    MooditTheme.colors.onSurfaceContainer,
+                    RoundedCornerShape(999.dp)
+                )
+                .padding(horizontal = 8.dp, vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = "icon_info",
+                modifier = Modifier.size(16.dp),
+                tint = MooditTheme.colors.onTertiary
+            )
+            Text(
+                text = "탭하거나 드래그해서 0.5점까지 선택할 수 있어요",
+                style = MooditTheme.typography.b3Medium,
+                color = MooditTheme.colors.textSecondary
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        MooditSlider(
+            value = currentSliderRating,
+            onValueChange = { onValueChange(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "1.0",
+                style = MooditTheme.typography.caption,
+                color = MooditTheme.colors.tertiary
+            )
+            Text(
+                text = "5.0",
+                style = MooditTheme.typography.caption,
+                color = MooditTheme.colors.tertiary
+            )
+        }
+        val satisfactionText = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = MooditTheme.colors.primary,
+                    fontStyle = MooditTheme.typography.h1.fontStyle,
+                    fontSize = MooditTheme.typography.h1.fontSize,
+                    fontWeight = MooditTheme.typography.h1.fontWeight
+                )
+            ) {
+                append("$currentSliderRating")
+            }
+
+            withStyle(
+                style = SpanStyle(
+                    color = MooditTheme.colors.textSecondary,
+                    fontStyle = MooditTheme.typography.b3Medium.fontStyle,
+                    fontSize = MooditTheme.typography.b3Medium.fontSize,
+                    fontWeight = MooditTheme.typography.b3Medium.fontWeight
+                )
+            ) {
+                append(" / 5.0")
+            }
+        }
+
+        Text(text = satisfactionText)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        MooditFilledButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                onCompleteClick()
+            },
+            text = "완료"
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF12141B)
+@Composable
+fun SatisfactionBottomSheetContentPreview() {
+    MooditTheme {
+        SatisfactionBottomSheetContent(
+            currentSliderRating = 2.0f,
+            onValueChange = {},
+            onCompleteClick = {}
+        )
+    }
+}
