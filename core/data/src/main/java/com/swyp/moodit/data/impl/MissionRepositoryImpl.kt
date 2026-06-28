e38481e3ee96712aca7d42844051d5c32037d38a
User <mk43313@gmail.com>
2026-06-28 23:00:31 +0900

    feat: MissionRepository 미션 상세 조회 기능 구현 (#25)

--- core/data/src/main/java/com/swyp/moodit/data/impl/MissionRepositoryImpl.kt
+++ core/data/src/main/java/com/swyp/moodit/data/impl/MissionRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.swyp.moodit.data.impl
 
+import com.swyp.moodit.common.util.Result
+import com.swyp.moodit.data.mapper.toModel
 import com.swyp.moodit.data.repository.MissionRepository
+import com.swyp.moodit.model.Mission
 import com.swyp.moodit.network.api.MooditApi
+import com.swyp.moodit.network.model.getOrThrow
 import javax.inject.Inject
 
 internal class MissionRepositoryImpl @Inject constructor(
     private val mooditApi: MooditApi
-): MissionRepository {
+) : MissionRepository {
+    override suspend fun getMissionDetail(userMissionId: Long): Result<Mission> {
+        return try {
+            val response = mooditApi.getMissionDetail(userMissionId).getOrThrow()
+            Result.Success(response.toModel())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
 }
--- core/data/src/main/java/com/swyp/moodit/data/repository/MissionRepository.kt
+++ core/data/src/main/java/com/swyp/moodit/data/repository/MissionRepository.kt
@@ -1,4 +1,8 @@
 package com.swyp.moodit.data.repository
 
+import com.swyp.moodit.common.util.Result
+import com.swyp.moodit.model.Mission
+
 interface MissionRepository {
+    suspend fun getMissionDetail(userMissionId: Long): Result<Mission>
 }
