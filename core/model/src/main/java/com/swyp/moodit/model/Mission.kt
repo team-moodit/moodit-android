ceed26cf42d56a0d41986ac94e26e466e9babcbc
User <mk43313@gmail.com>
2026-06-28 22:59:27 +0900

    feat: Mission 모델 생성 (#25)

--- core/model/src/main/java/com/swyp/moodit/model/Mission.kt
+++ core/model/src/main/java/com/swyp/moodit/model/Mission.kt
@@ -1,8 +1,16 @@
 package com.swyp.moodit.model
 
 data class Mission(
-    val id: Long,
-    val title: String,
-    val content: String,
-    val isCompleted: Boolean
+    val userMissionId: Long = 0L,
+    val title: String = "",
+    val missionState: MissionState = MissionState.IN_PROGRESS,
+    val imageUrl: String = "",
+    val roundCount: Int = 0,
+    val matchCompletedAt: String = ""
 )
+
+enum class MissionState {
+    IN_PROGRESS,
+    COMPLETED
+}
+
