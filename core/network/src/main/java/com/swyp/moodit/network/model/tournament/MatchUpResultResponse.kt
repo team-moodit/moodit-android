c1115783b10246583d0f9bed7f379fdabe66d0bd
User <mk43313@gmail.com>
2026-06-30 18:28:08 +0900

    feat: MatchUpResultResponse 모델 생성 (#21)

---
+++ core/network/src/main/java/com/swyp/moodit/network/model/tournament/MatchUpResultResponse.kt
@@ -0,0 +1,9 @@
+package com.swyp.moodit.network.model.tournament
+
+data class MatchUpResultResponse(
+    val matchResultId: Long,
+    val winnerPhotoId: Long,
+    val preferenceResultType: String,
+    val mainPreference: String,
+    val detailPreference: String
+)
