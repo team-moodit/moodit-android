12b9774016e539f73441f45c80b2cc99fac1e8fa
User <mk43313@gmail.com>
2026-07-03 05:06:16 +0900

    feat: ReportSummaryResponse -> ReportSummary  모델 매핑 함수 추가 (#37)

---
+++ core/data/src/main/java/com/swyp/moodit/data/mapper/ReportMapper.kt
@@ -0,0 +1,53 @@
+package com.swyp.moodit.data.mapper
+
+import com.swyp.moodit.model.report.PreferenceDetail
+import com.swyp.moodit.model.report.PreferenceReport
+import com.swyp.moodit.model.report.ReportSummary
+import com.swyp.moodit.model.report.SatisfactionSummary
+import com.swyp.moodit.model.report.SummaryCount
+import com.swyp.moodit.network.model.report.PreferenceDetailResponse
+import com.swyp.moodit.network.model.report.PreferenceReportResponse
+import com.swyp.moodit.network.model.report.ReportSummaryResponse
+import com.swyp.moodit.network.model.report.SatisfactionSummaryResponse
+import com.swyp.moodit.network.model.report.SummaryCountResponse
+
+fun PreferenceDetailResponse.toModel(): PreferenceDetail {
+    return PreferenceDetail(
+        type = this.type,
+        title = this.title,
+        selectedCount = this.selectedCount,
+        percentage = this.percentage
+    )
+}
+
+fun SummaryCountResponse.toModel(): SummaryCount {
+    return SummaryCount(
+        totalMatchCount = this.totalMatchCount,
+        completedMissionCount = this.completedMissionCount
+    )
+}
+
+fun SatisfactionSummaryResponse.toModel(): SatisfactionSummary {
+    return SatisfactionSummary(
+        count = this.count,
+        rate = this.rate,
+        minRate = this.minRate,
+        maxRate = this.maxRate
+    )
+}
+
+fun PreferenceReportResponse.toModel(): PreferenceReport {
+    return PreferenceReport(
+        totalSelectionCount = this.totalSelectionCount,
+        topPreference = this.topPreference.toModel(),
+        distributions = this.distributions.map { it.toModel() }
+    )
+}
+
+fun ReportSummaryResponse.toModel(): ReportSummary {
+    return ReportSummary(
+        summary = this.summary.toModel(),
+        preferenceReport = this.preferenceReport.toModel(),
+        rateSummary = this.rateSummary.toModel()
+    )
+}
