2dbd87eca7f9ed2bb09a17e3de6f60a508db35a1
User <mk43313@gmail.com>
2026-07-03 05:22:28 +0900

    feat: ReportRepository 취향 리포트 조회 로직 구현 (#37)

--- core/data/src/main/java/com/swyp/moodit/data/impl/ReportRepositoryImpl.kt
+++ core/data/src/main/java/com/swyp/moodit/data/impl/ReportRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.swyp.moodit.data.impl
 
+import com.swyp.moodit.common.util.Result
+import com.swyp.moodit.data.mapper.toModel
 import com.swyp.moodit.data.repository.ReportRepository
+import com.swyp.moodit.model.report.ReportSummary
 import com.swyp.moodit.network.api.MooditApi
+import com.swyp.moodit.network.model.getOrThrow
 import javax.inject.Inject
 
 class ReportRepositoryImpl @Inject constructor(
     private val mooditApi: MooditApi
 ) : ReportRepository {
+    override suspend fun getPreferenceReport(): Result<ReportSummary> {
+        return try {
+            val response = mooditApi.getReportSummary().getOrThrow()
+            Result.Success(response.toModel())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
 }
--- core/data/src/main/java/com/swyp/moodit/data/repository/ReportRepository.kt
+++ core/data/src/main/java/com/swyp/moodit/data/repository/ReportRepository.kt
@@ -1,4 +1,8 @@
 package com.swyp.moodit.data.repository
 
+import com.swyp.moodit.common.util.Result
+import com.swyp.moodit.model.report.ReportSummary
+
 interface ReportRepository {
+    suspend fun getPreferenceReport(): Result<ReportSummary>
 }
