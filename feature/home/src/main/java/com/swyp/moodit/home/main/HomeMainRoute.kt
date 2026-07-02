a28c2c3fa07a13b3b8e3576946ed1860d4154027
User <mk43313@gmail.com>
2026-07-02 16:32:14 +0900

    feat: HomeScreen 복귀 시 페이징 데이터 새로고침 구현 (#31)

--- feature/home/src/main/java/com/swyp/moodit/home/main/HomeMainRoute.kt
+++ feature/home/src/main/java/com/swyp/moodit/home/main/HomeMainRoute.kt
@@ -10,6 +10,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.swyp.moodit.designsystem.component.MooditSnackbarType
@@ -28,16 +30,10 @@ fun HomeMainRoute(
     val completedMissions = uiState.completedMissions.collectAsLazyPagingItems()
     val feedbackSubMittedMissions = uiState.feedbackSubMittedMissions.collectAsLazyPagingItems()
 
-    LaunchedEffect(inProgressMissions.loadState) {
+    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
         inProgressMissions.refresh()
-    }
-
-    LaunchedEffect(completedMissions.loadState) {
-        Log.d("PagingStatus", "completedMissionsLoadState: ${completedMissions.loadState.refresh}")
-    }
-
-    LaunchedEffect(feedbackSubMittedMissions.loadState) {
-        Log.d("PagingStatus", "feedbackSubMittedMissionsLoadState: ${feedbackSubMittedMissions.loadState.refresh}")
+        completedMissions.refresh()
+        feedbackSubMittedMissions.refresh()
     }
 
     LaunchedEffect(Unit) {
