ead3cc268bba35a3bc66ede8e5774dbb6d681bc8
User <mk43313@gmail.com>
2026-07-04 06:07:20 +0900

    refactor: MissionReviewedItemCard UI 모듈로 이동 (#37)

--- core/ui/src/main/java/com/swyp/moodit/ui/component/mission/MissionReviewedItemCard.kt
+++ core/ui/src/main/java/com/swyp/moodit/ui/component/mission/MissionReviewedItemCard.kt
@@ -1,4 +1,4 @@
-package com.swyp.moodit.home.component
+package com.swyp.moodit.ui.component.mission
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
