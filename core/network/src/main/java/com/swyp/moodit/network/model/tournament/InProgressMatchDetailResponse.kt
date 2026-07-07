d7df53aadd7068a242da0a4d96ae026995550f1b
User <mk43313@gmail.com>
2026-07-07 22:53:29 +0900

    feat: InProgressMatchDetailResponse 모델 생성 (#41)

---
+++ core/network/src/main/java/com/swyp/moodit/network/model/tournament/InProgressMatchDetailResponse.kt
@@ -0,0 +1,20 @@
+package com.swyp.moodit.network.model.tournament
+
+data class InProgressMatchDetailResponse(
+    val tournamentTitle: String,
+    val totalRounds: Int,
+    val currentRound: Int,
+    val currentMatchOrder: Int,
+    val matchInfo: InProgressMatchInfoResponse,
+    val selectedImages: List<MatchImageResponse>
+)
+
+data class InProgressMatchInfoResponse(
+    val totalImageCount: Int,
+    val createdAt: String
+)
+
+data class MatchImageResponse(
+    val id: Long,
+    val photoUri: String
+)
