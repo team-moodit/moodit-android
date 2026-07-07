b9b7c0ffe2229062f9bf02d72c0dc90b0636d94a
User <mk43313@gmail.com>
2026-07-08 03:24:55 +0900

    feat: CompletedMatchDetailResponse 모델 생성 (#41)

---
+++ core/network/src/main/java/com/swyp/moodit/network/model/tournament/CompletedMatchDetailResponse.kt
@@ -0,0 +1,15 @@
+package com.swyp.moodit.network.model.tournament
+
+data class CompletedMatchDetailResponse(
+    val title: String,
+    val winnerImage: MatchImageResponse,
+    val preferenceResult: PreferenceResultResponse,
+    val completedAt: String,
+    val selectedImages: List<MatchImageResponse>
+)
+
+data class PreferenceResultResponse(
+    val preferenceType: String,
+    val preferenceDetailType: String
+)
+
