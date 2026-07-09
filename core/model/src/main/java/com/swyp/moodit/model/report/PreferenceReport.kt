8026c8d5bc4038bfaa20fed979188dd1bb1f9124
User <mk43313@gmail.com>
2026-07-10 04:31:18 +0900

    feat: ReportMapper 함수 구현 (#44)

--- core/model/src/main/java/com/swyp/moodit/model/report/PreferenceReport.kt
+++ core/model/src/main/java/com/swyp/moodit/model/report/PreferenceReport.kt
@@ -2,6 +2,12 @@ package com.swyp.moodit.model.report
 
 data class PreferenceReport(
     val totalSelectionCount: Long = 0L,
+    val resultType: ResultType = ResultType.NONE,
     val topPreference: PreferenceDetail = PreferenceDetail(),
-    val distributions: List<PreferenceDetail> = emptyList()
+    val topPreferenceDetail: TopPreferenceDetail = TopPreferenceDetail(),
+    val distributions: List<Distribution> = emptyList()
 )
+
+enum class ResultType {
+    NONE, PREFERENCE_TIE, PREFERENCE_ONLY, PREFERENCE_DETAIL
+}
