948919aa1fa4baf559163666dbab146549e436a9
User <mk43313@gmail.com>
2026-07-11 08:16:18 +0900

    style: PrimaryRow 상태 표시줄에 출력되는 현상 수정 (#52)

--- feature/tournament/src/main/java/com/swyp/moodit/tournament/completedDetail/component/CompletedTournamentTabRow.kt
+++ feature/tournament/src/main/java/com/swyp/moodit/tournament/completedDetail/component/CompletedTournamentTabRow.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
@@ -20,38 +21,43 @@ import com.swyp.moodit.tournament.completedDetail.CompletedTournamentTab
 fun CompletedTournamentTabRow(
     selectedTab: CompletedTournamentTab, onTabClick: (CompletedTournamentTab) -> Unit
 ) {
-    PrimaryTabRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .systemBarsPadding(),
-        selectedTabIndex = selectedTab.ordinal,
-        containerColor = MooditTheme.colors.background,
-        contentColor = MooditTheme.colors.onPrimaryContainer,
-        divider = {
-            HorizontalDivider(
-                thickness = 1.dp, color = MooditTheme.colors.onPrimary
-            )
-        },
-        indicator = {
-            TabRowDefaults.PrimaryIndicator(
-                modifier = Modifier
-                    .tabIndicatorOffset(selectedTab.ordinal)
-                    .padding(horizontal = 56.dp),
-                width = Dp.Unspecified,
-                height = 2.dp,
-                color = MooditTheme.colors.primary
-            )
-        }) {
-        CompletedTournamentTab.entries.forEachIndexed { index, tab ->
-            Tab(
-                selected = selectedTab.ordinal == index,
-                onClick = { onTabClick(tab) },
-                text = {
-                    Text(text = tab.tabName, style = MooditTheme.typography.h4)
-                },
-                selectedContentColor = MooditTheme.colors.onPrimaryContainer,
-                unselectedContentColor = MooditTheme.colors.borderDefault,
-            )
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = MooditTheme.colors.background
+    ) {
+        PrimaryTabRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .systemBarsPadding(),
+            selectedTabIndex = selectedTab.ordinal,
+            containerColor = MooditTheme.colors.background,
+            contentColor = MooditTheme.colors.onPrimaryContainer,
+            divider = {
+                HorizontalDivider(
+                    thickness = 1.dp, color = MooditTheme.colors.onPrimary
+                )
+            },
+            indicator = {
+                TabRowDefaults.PrimaryIndicator(
+                    modifier = Modifier
+                        .tabIndicatorOffset(selectedTab.ordinal)
+                        .padding(horizontal = 56.dp),
+                    width = Dp.Unspecified,
+                    height = 2.dp,
+                    color = MooditTheme.colors.primary
+                )
+            }) {
+            CompletedTournamentTab.entries.forEachIndexed { index, tab ->
+                Tab(
+                    selected = selectedTab.ordinal == index,
+                    onClick = { onTabClick(tab) },
+                    text = {
+                        Text(text = tab.tabName, style = MooditTheme.typography.h4)
+                    },
+                    selectedContentColor = MooditTheme.colors.onPrimaryContainer,
+                    unselectedContentColor = MooditTheme.colors.borderDefault,
+                )
+            }
         }
     }
 }
