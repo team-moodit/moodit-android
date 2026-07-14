c71e94bf8d217bc70938a57081c5aca140e69ef0
User <mk43313@gmail.com>
2026-07-14 22:41:47 +0900

    feat: FirebaseAnalyticsHelper 구현 (#58)

---
+++ core/analytics/src/main/java/com/swyp/moodit/analytics/di/FirebaseAnalyticsHelper.kt
@@ -0,0 +1,22 @@
+package com.swyp.moodit.analytics.di
+
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.logEvent
+import com.swyp.moodit.analytics.AnalyticsEvent
+import com.swyp.moodit.analytics.AnalyticsHelper
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FirebaseAnalyticsHelper @Inject constructor(
+    private val firebaseAnalytics: FirebaseAnalytics
+) : AnalyticsHelper {
+
+    override fun logEvent(event: AnalyticsEvent) {
+        firebaseAnalytics.logEvent(event.type) {
+            event.extras.forEach { param ->
+                param(param.key, param.value)
+            }
+        }
+    }
+}
